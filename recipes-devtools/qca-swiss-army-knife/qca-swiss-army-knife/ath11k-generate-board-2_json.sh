8eb3acbdab02ad88cba4892c7d5168fd4aff062a
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2021-01-20 18:11:11 +0300

    qca-swiss-army-knife: add scripts to generate ath10k/ath11k board-2.json
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

---
+++ recipes-devtools/qca-swiss-army-knife/qca-swiss-army-knife/ath11k-generate-board-2_json.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+JSON="$2"
+
+iter=0
+echo "[" > "${JSON}"
+for file in $1/bdwlan.elf $1/bdwlan.e* ; do
+    iter=$((iter+1))
+    [ $iter -ne 1 ] && echo "  }," >> "${JSON}"
+
+    echo "  {" >> "${JSON}"
+    echo "          \"data\": \"$file\"," >> "${JSON}"
+    if [ `basename $file` = "bdwlan.elf" ]; then
+        file_ext="255"
+    else
+        file_ext="$(( $(basename "${file}" | sed -E 's:^.*\.e?([0-9a-f]*)$:0x\1:') ))"
+    fi
+    echo "          \"names\": [\"bus=pci,qmi-chip-id=0,qmi-board-id=${file_ext}\"]" >> "${JSON}"
+done
+
+echo "  }" >> "${JSON}"
+echo "]" >> "${JSON}"
+
