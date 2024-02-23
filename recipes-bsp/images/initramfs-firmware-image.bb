kraparthy/firmware-recipe

initramfs-firmware-package: enable RB3Gen2 firmware packages
Include RB3Gen2 firmware packages into the all-firmware image.

Signed-off-by: Kiran Raparthy <quic_kraparth@quicinc.com>
Change-Id: I7f568846f01b2245cc974b1f78956d6171429971

recipes-bsp/images/initramfs-firmware-image.bb
@@ -11,6 +11,7 @@ PACKAGE_INSTALL:qcom-armv8a = " \
     packagegroup-firmware-rb2 \
     packagegroup-firmware-rb5 \
     ${@bb.utils.contains('DISTRO_FEATURES', 'wifi', 'wireless-regdb-static', '', d)} \
+    packagegroup-firmware-rb3g2 \
 "
 
 BAD_RECOMMENDATIONS = " \

Linaro/meta-qcom

recipes-bsp/images/initramfs-firmware-image.bb
@@ -9,6 +9,7 @@ PACKAGE_INSTALL:qcom-armv8a = " \
     packagegroup-firmware-dragonboard845c \
     packagegroup-firmware-rb1 \
     packagegroup-firmware-rb2 \
+    packagegroup-firmware-rb3gen2 \
     packagegroup-firmware-rb5 \
     ${@bb.utils.contains('DISTRO_FEATURES', 'wifi', 'wireless-regdb-static', '', d)} \
 "